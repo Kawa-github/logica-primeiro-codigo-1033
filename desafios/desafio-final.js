3d77497fcf2a1bf1dd9a050d5f8ea1e706c2f2e1
Kawa Ribeiro <kawa.rfaragao@gmail.com>
2023-08-14 12:10:55 -0300

    atualizar turmas e aluno

--- desafios/desafio-final.js
+++ desafios/desafio-final.js
@@ -22,6 +22,20 @@ function criarTurma(codigo, maxAlunos) {
     turmas.push({ codigo, maxAlunos })
 }
 
+function atualizarTurma(codigo, novoMaxAlunos) {
+    if (novoMaxAlunos < 5 || novoMaxAlunos > 10) {
+        throw new Error('O novo máximo de alunos está fora do escopo planejado.')
+    }
+
+    const turmaIndex = turmas.findIndex(turma => turma.codigo === codigo)
+
+    if (turmaIndex === -1) {
+        throw new Error(`Não foi encontrada uma turma com o código ${codigo}.`)
+    }
+
+    turmas[turmaIndex].maxAlunos = novoMaxAlunos
+}
+
 try {
     criarTurma(1, 10)
     criarTurma(2, 8)
@@ -30,9 +44,11 @@ try {
     // criarTurma(8, 8)
     // criarTurma(3, 8)
     // criarTurma(8, 1)
+
+    atualizarTurma(1, 5)
     
 } catch (erro) {
-    console.error(erro.message);
+    console.error(erro.message)
 }
 finally{
     console.log(turmas)
@@ -50,7 +66,7 @@ function criarAluno(nome,sobrenome,email,turma,nascimento,notas,ativo = true){
     let mes = dataNascimento.getMonth() + 1 
     let ano = dataNascimento.getFullYear()
 
-    let dataFormatada = dia + '/' + mes + '/' + ano;
+    let dataFormatada = dia + '/' + mes + '/' + ano
 
     const alunoRepetido = alunos.find(aluno => aluno.email === email)
 
@@ -143,7 +159,71 @@ criarAluno(
     1
 )
 
-console.log(alunos)
+// console.log(alunos)
+function atualizarAluno(novoNome, novoSobrenome, novoEmail,  novaTurma, novoNascimento, novasNotas, novoAtivo) {
+    const alunoIndex = alunos.findIndex(aluno => aluno.email === novoEmail)
+
+    if (alunoIndex === -1) {
+        throw new Error(`Não foi encontrado um aluno com o e-mail ${novoEmail}.`)
+    }
+
+    if (novaTurma < 1 || novaTurma > 10) {
+        throw new Error('Turma inválida!')
+    }
+
+    const emailRepetido = alunos.find((aluno, index) => aluno.novoEmail === novoEmail && index !== alunoIndex)
+
+    if (emailRepetido) {
+        throw new Error('Já existe um aluno com este e-mail.')
+    }
+
+    if (!novoEmail.includes("@")) {
+        throw new Error('Email inválido!')
+    }
+
+    let dataNascimento = new Date(novoNascimento)
+    let dia = dataNascimento.getDate()
+    let mes = dataNascimento.getMonth() + 1
+    let ano = dataNascimento.getFullYear()
+    let dataFormatada = dia + '/' + mes + '/' + ano
+
+    let nomeFormat = novoNome.charAt(0).toUpperCase() + novoNome.slice(1)
+    
+    alunos[alunoIndex] = {
+        nome: nomeFormat.trim(),
+        sobrenome: novoSobrenome,
+        novoEmail: novoEmail,
+        turma: novaTurma,
+        nascimento: dataFormatada,
+        ativo: novoAtivo === 1 ? true : false,
+        notas: novasNotas.map(nota => ({ nota: nota })),
+        calcularMedia: function () {
+            const somarNotas = this.notas.reduce((acc, current) => {
+                return acc + current.nota
+            }, 0)
+            const media = somarNotas / 6
+
+            return media
+        },
+    }
+}
+
+try {
+    atualizarAluno(
+        "Roger",
+        "Guedes",
+        "lulu@gmail.com",
+        3,
+        "2000-01-03",
+        [7, 8, 9, 10],
+        0
+    )
+} catch (erro) {
+    console.error(erro.message)
+} finally {
+    console.log(alunos)
+}
+
 
 function removerAluno(alunos, prop, value) {
     return alunos.filter((a) => a[prop] !== value)
